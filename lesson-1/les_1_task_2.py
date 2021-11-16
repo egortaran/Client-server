468396fc0229f93cbec6a252faf74d0ed075de90
Egor Taran <egortaran@mail.ru>
2021-11-16 11:24:00 +0600

    add 2-4

---
+++ lesson-1/les_1_task_2.py
@@ -0,0 +1,4 @@
+words = [b"class", b"function", b"method"]
+
+for word in words:
+    print(f'{word}: {type(word)}, len {len(word)}')
---
+++ lesson-1/les_1_task_4.py
@@ -0,0 +1,5 @@
+words = ["разработка", "администрирование", "protocol", "standard"]
+
+for i in range(len(words)):
+    words[i] = words[i].encode('utf-8')
+    words[i] = words[i].decode('utf-8')
