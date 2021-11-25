9448a13199864734ad602afb4e53cf3fa3e59f26
Egor Taran <egortaran@mail.ru>
2021-11-26 00:22:17 +0600

    lesson-3

---
+++ lesson-3/client_func/create_presence.py
@@ -0,0 +1,18 @@
+import time
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME
+
+
+def create_presence(account_name='Guest'):
+    """
+    Функция генерирует запрос о присутствии клиента
+    :param account_name:
+    :return:
+    """
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
