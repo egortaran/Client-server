8221c09b4f6a4ca528258438eda6de85640272c5
Egor Taran <egortaran@mail.ru>
2021-12-05 13:38:46 +0600

    project

--- project/common/variables.py
+++ project/common/variables.py
@@ -1,5 +1,7 @@
 """Константы"""
 
+import logging
+
 # Порт поумолчанию для сетевого ваимодействия
 DEFAULT_PORT = 7777
 # IP адрес по умолчанию для подключения клиента
@@ -7,9 +9,11 @@ DEFAULT_IP_ADDRESS = '127.0.0.1'
 # Максимальная очередь подключений
 MAX_CONNECTIONS = 5
 # Максимальная длинна сообщения в байтах
-MAX_PACKAGE_LENGTH = 2048
+MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
+# Текущий уровень логирования
+LOGGING_LEVEL = logging.DEBUG
 
 # Прококол JIM основные ключи:
 ACTION = 'action'
@@ -20,11 +24,4 @@ ACCOUNT_NAME = 'account_name'
 # Прочие ключи, используемые в протоколе
 PRESENCE = 'presence'
 RESPONSE = 'response'
-MESSAGE = 'message'
-MSG = 'msg'
-FROM = 'from'
-TO = 'to'
-PASSWORD = 'password'
-AUTHENTICATE = 'authenticate'
 ERROR = 'error'
-RESPONDEFAULT_IP_ADDRESSSE = 'respondefault_ip_addressse'
