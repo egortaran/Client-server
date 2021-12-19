b1862ca98e607a73a55c4c2d057c375b32198aea
Egor Taran <64767049+egortaran@users.noreply.github.com>
2021-12-19 20:33:41 +0600

    Update les_2.1_task_2.py

--- lesson-2.1/les_2.1_task_2.py
+++ lesson-2.1/les_2.1_task_2.py
@@ -1,11 +1,13 @@
 from ipaddress import ip_address
 
-IP1 = ip_address('192.168.1.0')
-IP2 = '192.168.1.' + input('Введите диапозон адрессов от 0 до 255: ')
 
-try:
-    IP2 = ip_address(IP2)
-    for i in range(int(IP2) - int(IP1) + 1):
-        print(IP1 + i)
-except ValueError:
-    print('Вы ввели неправильный диапозон')
+def host_range_ping():
+    IP1 = ip_address('192.168.1.0')
+    IP2 = '192.168.1.' + input('Введите диапозон адрессов от 0 до 255: ')
+
+    try:
+        IP2 = ip_address(IP2)
+        for i in range(int(IP2) - int(IP1) + 1):
+            print(IP1 + i)
+    except ValueError:
+        print('Вы ввели неправильный диапозон')
